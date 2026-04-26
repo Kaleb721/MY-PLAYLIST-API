a52499d10863fb3b15b9685662dd03d47401b69b
Caleb Mitiku <kalebmitiku55@gmail.com>
2026-04-26 16:38:32 +0300

    second commit

--- server.js
+++ server.js
@@ -13,3 +13,47 @@ function ensureDataFile() {
         fs.writeFileSync(DATA_FILE, JSON.stringify([]));
     }
 }
+function readData() {
+    try {
+        ensureDataFile();
+        const data = fs.readFileSync(DATA_FILE, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        return [];
+    }
+}
+function writeData(data) {
+    try {
+        fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+function getNextId(songs) {
+    if (songs.length === 0) return 1;
+    let maxId = 0;
+    for (let i = 0; i < songs.length; i++) {
+        if (songs[i].id > maxId) {
+            maxId = songs[i].id;
+        }
+    }
+    return maxId + 1;
+}
+function parseBody(req) {
+    return new Promise((resolve, reject) => {
+        let body = '';
+        req.on('data', chunk => body += chunk.toString());
+        req.on('end', () => {
+            try {
+                resolve(body ? JSON.parse(body) : {});
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+}
+function sendJSON(res, statusCode, data) {
+    res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data));
+}
